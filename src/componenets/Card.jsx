52fc92533e450238f15eecce25a77513dfaa9d66
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-21 14:09:53 +0300

    .

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -1,27 +1,29 @@
 export function Card(props) {
+  function dataOrDefault() {
+    const defaultLink =
+      "https://books.google.co.il/googlebooks/images/no_cover_thumb.gif";
+    const imgProp = props.item.volumeInfo.imageLinks.thumbnail;
+
+    if (imgProp) {
+      return imgProp;
+    } else return defaultLink;
+  }
+
   return (
     <>
       <div className="centered">
-        <img
-          alt=" Book Title"
-          src={props.item.volumeInfo.imageLinks.thumbnail}
-          onError={(e) => {
-            e.target.onerror = null;
-            e.target.src = "no_cover_thumb.gif";
-          }}
-        />
+        <img alt=" Book Title" src={dataOrDefault()} />
 
         <div
-          numberOfLines={1}
           className="center"
           style={{ backgroundColor: "rgba(251, 251, 1, 0.2)" }}
         ></div>
         <ul>
-          <li numberOfLines={0} ellipsizeMode="tail">
+          <li numberOfLines={1} ellipsizemode="tail">
             Author: {props.item.volumeInfo.authors || "Author Name"}
           </li>
 
-          <li numberOfLines={1} ellipsizeMode="tail">
+          <li numberOfLines={1} ellipsizemode="tail">
             Title: {props.item.volumeInfo.title || "Title Name"}
           </li>
         </ul>
