68ff65202cd8c0a8d34ad2d245c220248856dfca
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-21 17:22:33 +0300

    .

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -11,6 +11,7 @@ export function Card(props) {
   return (
     <div className="centered" resizeMode="cover" borderRadius={20}>
       <img
+        className="rounded-top"
         resizeMode="cover"
         borderRadius={20}
         alt=" Book Title"
@@ -24,11 +25,13 @@ export function Card(props) {
         style={{ backgroundColor: "rgba(251, 251, 1, 0.2)" }}
       ></div>
       <ul>
-        <li numberOfLines={1} ellipsizemode="tail" className="">
+        <li numberOfLines={1} ellipsizemode="tail" className="" maxlength="8">
           Author: {props.item.volumeInfo.authors || "Author Name"}
         </li>
 
-        <li numberOfLines={1} ellipsizemode="tail">
+        <li numberOfLines={1} ellipsizemode="tail"
+        vmaxlength="8"
+        >
           Title: {props.item.volumeInfo.title || "Title Name"}
         </li>
       </ul>
