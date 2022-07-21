2db84335dadf0642546b79f6e6e0377c289562fc
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-21 18:02:09 +0300

    .

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -1,16 +1,9 @@
 export function Card(props) {
   function dataOrDefault() {
-    const shortenText = (str, len) => {
-      if (str.length > len) {
-        if (len <= 10) {
-          return str.slice(0, len - 10) + "...";
-        } else {
-          return str.slice(0, len) + "...";
-        }
-      } else {
-        return str;
-      }
-    };
+    
+    function truncate(str) {
+      return str.length > 10 ? str.substring(0, 7) + "..." : str;
+  }
     const defaultLink =
       "https://books.google.co.il/googlebooks/images/no_cover_thumb.gif";
 
@@ -23,27 +16,27 @@ export function Card(props) {
     <div className="centered" resizeMode="cover" borderRadius={20}>
       <img
         className="rounded-top"
+
         resizeMode="cover"
         borderRadius={20}
         alt=" Book Title"
+        style={{overflow: 'hidden', textOverflow: 'ellipsis'}}
         src={dataOrDefault()}
       />
 
       <div
         resizeMode="cover"
         borderRadius={30}
-        className=""
+        className="truncate"
         style={{ backgroundColor: "rgba(251, 251, 1, 0.2)" }}
       ></div>
-      <ul>
-        <p numberOfLines={1} ellipsizemode="tail" className="" maxlength="4">
+        <p numberOfLines={1} ellipsizemode="tail" className="truncate" maxlength="4" >
           Author: {props.item.volumeInfo.authors || "Author Name"}
         </p>
 
-        <p numberOfLines={1} ellipsizemode="tail" vmaxlength="8">
+        <p numberOfLines={1} ellipsizemode="tail" vmaxlength="8" className="truncate">
           Title: {props.item.volumeInfo.title || "Title Name"}
         </p>
-      </ul>
     </div>
   );
 }
